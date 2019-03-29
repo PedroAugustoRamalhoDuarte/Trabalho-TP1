25f3c90ceba2aef73f477733a855dd80f8ebe83d
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-03-29 00:21:27 -0300

    Segunda Ideia de definicao dos Dominios, e teste dela

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+#include "DominiosIdeia2.h"
+#include <stdio.h>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // n pode b
+    Data data;
+    // n pode a
+    NomeDeEvento nomeDeEvento;
+    data.setValor("bb");
+    nomeDeEvento.setValor("aa");
     return 0;
 }
