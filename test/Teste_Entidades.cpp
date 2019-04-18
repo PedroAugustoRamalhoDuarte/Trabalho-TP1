e9bf4a79d9c1b064901a4d532b0ca769265a6017
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-04-18 11:23:04 -0300

    Testes Entidades.cpp concluidos

--- test/Teste_Entidades.cpp
+++ test/Teste_Entidades.cpp
@@ -29,7 +29,7 @@ TEST_CASE("Teste Evento") {
     Evento evento;
     SECTION("Teste Set e Get codigo"){
         CodigoDeEvento codigoDeEvento;
-        string codigoValido = "";
+        string codigoValido = "456";
         codigoDeEvento.setValor(codigoValido);
         evento.setCodigo(codigoDeEvento);
         REQUIRE(evento.getCodigo().getValor() == codigoValido);
@@ -38,15 +38,15 @@ TEST_CASE("Teste Evento") {
 
     SECTION("Teste Set e Get nome"){
         NomeDeEvento nomeDeEvento;
-        string nomeValido = "";
+        string nomeValido = "Computaiada";
         nomeDeEvento.setValor(nomeValido);
-        evento.setNome(nomeDeEvento;
+        evento.setNome(nomeDeEvento);
         REQUIRE(evento.getNome().getValor() == nomeValido);
     }
 
     SECTION("Teste Set e Get cidade"){
         Cidade cidade;
-        string cidadeValida = "";
+        string cidadeValida = "Brasilia";
         cidade.setValor(cidadeValida);
         evento.setCidade(cidade);
         REQUIRE(evento.getCidade().getValor() == cidadeValida);
@@ -54,7 +54,7 @@ TEST_CASE("Teste Evento") {
 
     SECTION("Teste Set e Get estado"){
         Estado estado;
-        string estadoValido = "";
+        string estadoValido = "RN";
         estado.setValor(estadoValido);
         evento.setEstado(estado);
         REQUIRE(evento.getEstado().getValor() == estadoValido);
@@ -62,7 +62,7 @@ TEST_CASE("Teste Evento") {
 
     SECTION("Teste Set e Get Classe"){
         ClasseDeEvento classeDeEvento;
-        string classeValida = "";
+        string classeValida = "2";
         classeDeEvento.setValor(classeValida);
         evento.setClasse(classeDeEvento);
         REQUIRE(evento.getClasse().getValor() == classeValida);
@@ -70,7 +70,7 @@ TEST_CASE("Teste Evento") {
 
     SECTION("Teste Set e Get Faixa"){
         FaixaEtaria faixaEtaria;
-        string faixaValida = "";
+        string faixaValida = "L";
         faixaEtaria.setValor(faixaValida);
         evento.setFaixa(faixaEtaria);
         REQUIRE(evento.getFaixa().getValor() == faixaValida);
@@ -81,7 +81,7 @@ TEST_CASE("Teste Apresentacao") {
     Apresentacao apresentacao;
     SECTION("Teste Set e Get codigo"){
         CodigoDeApresentacao codigoDeApresentacao;
-        string codigoValido = "";
+        string codigoValido = "6532";
         codigoDeApresentacao.setValor(codigoValido);
         apresentacao.setCodigo(codigoDeApresentacao);
         REQUIRE(apresentacao.getCodigo().getValor() == codigoValido);
@@ -90,7 +90,7 @@ TEST_CASE("Teste Apresentacao") {
 
     SECTION("Teste Set e Get data"){
         Data data;
-        string dataValida = "";
+        string dataValida = "23/12/36";
         data.setValor(dataValida);
         apresentacao.setData(data);
         REQUIRE(apresentacao.getData().getValor() == dataValida);
@@ -98,7 +98,7 @@ TEST_CASE("Teste Apresentacao") {
 
     SECTION("Teste Set e Get horario"){
         Horario horario;
-        string horarioValido = "";
+        string horarioValido = "10:15";
         horario.setValor(horarioValido);
         apresentacao.setHorario(horario);
         REQUIRE(apresentacao.getHorario().getValor() == horarioValido);
@@ -106,7 +106,7 @@ TEST_CASE("Teste Apresentacao") {
 
     SECTION("Teste Set e Get preco"){
         Preco preco;
-        string precoValido = "";
+        string precoValido = "500";
         preco.setValor(precoValido);
         apresentacao.setPreco(preco);
         REQUIRE(apresentacao.getPreco().getValor() == precoValido);
@@ -114,7 +114,7 @@ TEST_CASE("Teste Apresentacao") {
 
     SECTION("Teste Set e Get numeroDeSala"){
         NumeroDeSala numeroDeSala;
-        string numeroDeSalaValido = "";
+        string numeroDeSalaValido = "6";
         numeroDeSala.setValor(numeroDeSalaValido);
         apresentacao.setNumeroDeSala(numeroDeSala);
         REQUIRE(apresentacao.getNumeroDeSala().getValor() == numeroDeSalaValido);
@@ -122,7 +122,7 @@ TEST_CASE("Teste Apresentacao") {
     
     SECTION("Teste Set e Get Disponibilidade"){
         Disponibilidade disponibilidade;
-        string disponibilidadeValida = "";
+        string disponibilidadeValida = "100";
         disponibilidade.setValor(disponibilidadeValida);
         apresentacao.setDisponibilidade(disponibilidade);
         REQUIRE(apresentacao.getDisponibilidade().getValor() == disponibilidadeValida);
@@ -134,7 +134,7 @@ TEST_CASE("Teste Ingresso") {
     
     SECTION("Teste Set e Get codigo"){
         CodigoDeIngresso codigoDeIngresso;
-        string codigoValido = "";
+        string codigoValido = "54623";
         codigoDeIngresso.setValor(codigoValido);
         ingresso.setCodigo(codigoDeIngresso);
         REQUIRE(ingresso.getCodigo().getValor() == codigoValido);
@@ -146,7 +146,7 @@ TEST_CASE("Teste Cartao de Credito") {
 
     SECTION("Teste Set e Get numero"){
         NumeroDeCartaoDeCredito numero;
-        string numeroValido = "";
+        string numeroValido = "4984088007587535";
         numero.setValor(numeroValido);
         cartao.setNumero(numero);
         REQUIRE(cartao.getNumero().getValor() == numeroValido);
@@ -154,7 +154,7 @@ TEST_CASE("Teste Cartao de Credito") {
     
     SECTION("Teste Set e Get codigoDeSeguranca"){
         CodigoDeSeguranca codigoDeSeguranca;
-        string codigoValido = "";
+        string codigoValido = "834";
         codigoDeSeguranca.setValor(codigoValido);
         cartao.setCodigoDeSeguranca(codigoDeSeguranca);
         REQUIRE(cartao.getCodigoDeSeguranca().getValor() == codigoValido);
@@ -162,9 +162,9 @@ TEST_CASE("Teste Cartao de Credito") {
     
     SECTION("Teste Set e Get DataDeValidade"){
         DataDeValidade dataDeValidade;
-        string dataDeValidadeValida = "";
+        string dataDeValidadeValida = "03/21";
         dataDeValidade.setValor(dataDeValidadeValida);
-        dataDeValidade.setValor(dataDeValidadeValida);
+        cartao.setDataDeValidade(dataDeValidade);
         REQUIRE(cartao.getDataDeValidade().getValor() == dataDeValidadeValida);
     }
 }
